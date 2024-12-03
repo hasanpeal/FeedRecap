a754a41a81a68ff39c4b54ecac7b2f7bece82bf0
Peal Hasan <pealh0320@gmail.com>
2024-12-02 23:38:03 -0500

    Bug fixed, report problem added

--- client/components/navbar3.tsx
+++ client/components/navbar3.tsx
@@ -1,26 +1,30 @@
 "use client";
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useEmail } from "@/context/UserContext"; // Assuming you're using a context for email
+import { useEmail } from "@/context/UserContext";
 import axios from "axios";
+import emailjs from "@emailjs/browser";
 import { Toaster, toast } from "react-hot-toast";
 
 export default function Navbar2() {
   const router = useRouter();
-  const { emailContext, setEmailContext } = useEmail(); // Assuming you're using this from context
-  const [firstName, setFirstName] = useState<string>(""); // Ensure it initializes as an empty string
-  const [lastName, setLastName] = useState<string>(""); // Ensure it initializes as an empty string
-  const [email, setEmail] = useState<string>(emailContext || ""); // Initialize with the context email or empty string
+  const { emailContext, setEmailContext } = useEmail();
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>(emailContext || "");
+  const form = useRef<HTMLFormElement>(null);
 
-  // Fetch user details on load
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+
   useEffect(() => {
     if (emailContext) {
       fetchUserDetails();
     }
   }, [emailContext]);
 
-  // Fetch user details from backend and populate the form
   const fetchUserDetails = async () => {
     try {
       const response = await axios.get(
@@ -28,21 +32,19 @@ export default function Navbar2() {
         { params: { email: emailContext } }
       );
       if (response.data.code === 0) {
-        setFirstName(response.data.firstName || ""); // Set to empty string if undefined
-        setLastName(response.data.lastName || ""); // Set to empty string if undefined
-        setEmail(emailContext); // Email already available from context
+        setFirstName(response.data.firstName || "");
+        setLastName(response.data.lastName || "");
+        setEmail(emailContext);
       } else {
         toast.error("Error fetching user details.");
       }
     } catch (err) {
-      console.log("Error fetching user details:", err);
+      console.error("Error fetching user details:", err);
       toast.error("Error fetching user details.");
     }
   };
 
-  // Handle account update logic
   const handleAccountUpdate = async () => {
-    // Check if all fields are valid before sending the update request
     if (!firstName || !lastName || !email) {
       toast.error("Please ensure all fields are filled.");
       return;
@@ -52,27 +54,26 @@ export default function Navbar2() {
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_SERVER}/updateAccount`,
         {
-          email: emailContext, // Old email to identify the user
+          email: emailContext,
           newFirstName: firstName,
           newLastName: lastName,
-          newEmail: email, // The updated email (can be the same or changed)
+          newEmail: email,
         }
       );
 
-      if (response.data.code == 0) {
+      if (response.data.code === 0) {
         toast.success("Account updated successfully");
-        setEmailContext(email); // Update email context if email changes
-        localStorage.setItem("email", email); // Update localStorage email
+        setEmailContext(email);
+        localStorage.setItem("email", email);
       } else {
         toast.error(response.data.message);
       }
     } catch (err) {
-      console.log(err);
+      console.error(err);
       toast.error("Error updating account.");
     }
   };
 
-  // Handle logout logic
   const handleLogout = async () => {
     try {
       const response = await axios.post(
@@ -82,7 +83,6 @@ export default function Navbar2() {
       );
       if (response.data.code === 0) {
         toast.success("Logged out successfully.");
-        // Clear localStorage items like cookieConsent
         localStorage.removeItem("cookieConsent");
         localStorage.removeItem("email");
         router.push("/");
@@ -90,9 +90,39 @@ export default function Navbar2() {
         toast.error("Error logging out.");
       }
     } catch (err) {
-      console.log("Error during logout:", err);
+      console.error("Error during logout:", err);
       toast.error("Error logging out.");
     }
+  };
+
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    const modal = document.getElementById("report_modal") as HTMLDialogElement;
+    if (modal) {
+      modal.close();
+    }
+
+    console.log(form.current);
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID || "",
+        process.env.NEXT_PUBLIC_TEMPLATE_ID || "",
+        form.current,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY || ""
+      )
+      .then(
+        () => {
+          toast.success("Report submitted successfully");
+        },
+        (error) => {
+          console.error("Error sending email:", error.text);
+          toast.error("Failed to submit the report");
+        }
+      );
   };
 
   return (
@@ -100,10 +130,22 @@ export default function Navbar2() {
       <Toaster />
       <div className="py-4 px-6 flex">
         <Link className="text-xl font-bold text-gray-800 flex-grow" href="/">
-          FeedRecap{" "}
+          FeedRecap
         </Link>
         <nav className="space-x-4 buttonss">
-          {/* Account button opens the account modal */}
+          <button
+            className="text-gray-800 font-semibold"
+            onClick={() => {
+              const modal = document.getElementById(
+                "report_modal"
+              ) as HTMLDialogElement;
+              if (modal) {
+                modal.showModal();
+              }
+            }}
+          >
+            Report a Problem
+          </button>
           <button
             className="text-gray-800 font-semibold"
             onClick={() => {
@@ -117,8 +159,6 @@ export default function Navbar2() {
           >
             Account
           </button>
-
-          {/* Logout button */}
           <button
             className="text-gray-800 font-semibold"
             onClick={handleLogout}
@@ -189,6 +229,42 @@ export default function Navbar2() {
           >
             Close
           </button>
+        </div>
+      </dialog>
+
+      {/* Report Problem Modal */}
+      <dialog id="report_modal" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <form ref={form} onSubmit={sendEmail}>
+            <label className="block text-left mb-2">Name</label>
+            <input
+              type="text"
+              name="user_name"
+              className="input input-bordered w-full mb-4"
+              required
+            />
+            <label className="block text-left mb-2">Email</label>
+            <input
+              type="email"
+              name="user_email"
+              className="input input-bordered w-full mb-4"
+              required
+            />
+            <label className="block text-left mb-2">Message</label>
+            <textarea
+              name="message"
+              className="textarea textarea-bordered w-full mb-4"
+              required
+            ></textarea> 
+            <button className="btn btn-primary w-full mt-2" type="submit">
+              Submit Report
+            </button>
+          </form>
         </div>
       </dialog>
     </header>
